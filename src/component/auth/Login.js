5e692099401ee3295f4d47c7ce4a7692c981808e
abhicrypt <abhicrypt97@gmail.com>
2024-01-18 17:02:54 +0530

    test

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Captcha from './Captcha';
 import './LoginForm.css';
+import { Link, Redirect } from 'react-router-dom'
 
 const LoginForm = ({ onLogin }) => {
   const [email, setEmail] = useState('');
@@ -19,7 +20,10 @@ const LoginForm = ({ onLogin }) => {
   };
 
   return (
+    <>
+    <h2 className='brand-text text-primary mx-3 mt-2'>Abhishek.Dev</h2>
     <div className='container'>
+
     <div className="login-form">
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
@@ -34,18 +38,29 @@ const LoginForm = ({ onLogin }) => {
         </>
         <>
         <label>Password:</label>
+        <href to='/'>
+                    <small>Forgot Password?</small>
+                  </href>
         <input
           type="password"
         //   value={password}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+       
         </>
         <Captcha value={captchaValue} onChange={setCaptchaValue} />
         <button type="submit">Login</button>
       </form>
+      <p className='text-center mt-2'>
+              <span className='mr-25'>New on our platform?</span>
+              < >
+                <span>Create an account</span>
+              </>
+            </p>
     </div>
     </div>
+    </>
   );
 };
 
